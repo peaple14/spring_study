8f93bb2a3cdfb3d37721f0a096986e191218f7a4
Jojaerim <whwo1214@gmail.com>
2022-12-12 16:28:10 +0900

    20221212

--- src/main/java/mixptc/service/web/basic/BasicItemController.java
+++ src/main/java/mixptc/service/web/basic/BasicItemController.java
@@ -1,18 +1,20 @@
 package mixptc.service.web.basic;
 
+
 import lombok.RequiredArgsConstructor;
 import mixptc.service.domain.item.Item;
 import mixptc.service.domain.item.ItemRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
-@RequestMapping("/basic/items") //중복되는 주소들
+@RequestMapping("/basic/items")
 @RequiredArgsConstructor //생성자 주입용
 public class BasicItemController {
     //생성자만들기
@@ -30,7 +32,40 @@ public class BasicItemController {
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
-        model.addAttribute("item,item");
+        model.addAttribute("item","item");
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {return "basic/addForm";}
+
+    @PostMapping("/add")
+    public String addItem(Item item, RedirectAttributes redirectAttributes) {
+        Item saveItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", saveItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item,item");
+        return "basic/editForm";
+    }
+
+    @PostMapping("{itemId}/ edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
     }
 }
