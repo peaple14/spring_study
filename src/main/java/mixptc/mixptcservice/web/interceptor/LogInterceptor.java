583ebf45108f7b49e2cda557a8077a20942fa966
peaple14 <whwo1214@gmail.com>
2023-05-12 16:27:16 +0900

    230512 aftercompetion부분 로그 안남는것 수정.

--- src/main/java/mixptc/mixptcservice/web/interceptor/LogInterceptor.java
+++ src/main/java/mixptc/mixptcservice/web/interceptor/LogInterceptor.java
@@ -1,5 +1,6 @@
 package mixptc.mixptcservice.web.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
+@Slf4j
 public class LogInterceptor implements HandlerInterceptor {
 
     public static final String LOG_ID = "logId";
@@ -35,6 +37,7 @@ public class LogInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,Exception ex) throws Exception{
         String requestURI = request.getRequestURI();
         String logId = (String) request.getAttribute(LOG_ID);
+        log.info("Request URI : {}, Log ID : {}", requestURI, logId);
 
     }
 }
