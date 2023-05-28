6461fc977e1e0a6068c2d8b7336281eb2c10f5bf
peaple14 <whwo1214@gmail.com>
2023-05-28 19:59:19 +0900

    2023-05-28 JPA기본

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -16,7 +16,10 @@ import javax.sql.DataSource;
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(JdbcTemplateV3Config.class)
+//@Import(JdbcTemplateV3Config.class)
+//@Import(MybatisConfig.class)
+//@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @Slf4j
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,16 +2,25 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+//JPA에서 관리하고있다는뜻.
+@Entity
 public class Item {
 
+    //@ID=pk라는뜻.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//db에서 값이 증가하는것.id++
     private Long id;
 
+    //db에서 이런것들과 매핑된다는뜻.,camel과 언더스코어 자동변환.
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
-    public Item() {
+    public Item() { //JPA는 기본생성자 필수.
     }
 
     public Item(String itemName, Integer price, Integer quantity) {
